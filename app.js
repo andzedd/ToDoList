3e0c9666d5ea6a9567691b1f21704e395e35d058
Matheus S Souza <andzedd.matheus@gmail.com>
2020-10-29 15:25:40 -0300

    adding mongodb database

--- app.js
+++ app.js
@@ -1,19 +1,42 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js")
+const mongoose = require("mongoose");
 
 const app = express();
 const port = 3000;
-
-const items = [];
-const workItems = [];
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+const itemSchema = new mongoose.Schema({
+    name: String
+})
+
+const Item = new mongoose.model("Item", itemSchema);
+
+const item1 = new Item ({
+    name: "Welcome to your ToDoList"
+})
+
+const item2 = new Item ({
+    name: "Hit the + button to add a new item."
+})
+
+const item3 = new Item ({
+    name: "<-- Hit this to delete an item."
+})
+
+const defaultItems = [item1, item2, item3];
+
+Item.insertMany(defaultItems, err => {
+    err ? console.log(err) : console.log("Successfully added default items");
+})
+
 app.get("/", function(req,res){            
-    res.render('list', {listTitle: date.getDate(), newListItems: items})
+    res.render('list', {listTitle: "Today", newListItems: items})
 })
 
 app.get("/work", function(req,res){
